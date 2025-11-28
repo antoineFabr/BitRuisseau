164a19a169a73b7ff91e98db5a41ef845aa29914
Antoine <163823977+antoineFabr@users.noreply.github.com>
2025-11-28 11:19:44 +0100

    Feat: enregistrement des catalogs des autres personnes sur le réseau
    [DONE][30]

---
+++ src/BitRuisseau/BitRuisseau/data/Catalog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitRuisseau.data
+{
+    public class Catalog
+    {
+        public List<ISong> sons { get; set; }
+
+        public string holder { get; set; }
+    }
+}
