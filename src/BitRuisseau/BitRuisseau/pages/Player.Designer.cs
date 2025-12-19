b139d572f11f4985017f0da43064841a53b8ac00
Antoine <163823977+antoineFabr@users.noreply.github.com>
2025-12-19 10:55:41 +0100

    Feat: Ajout d'une page de player de musique
    [DONE][70]

---
+++ src/BitRuisseau/BitRuisseau/pages/Player.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BitRuisseau
+{
+    partial class Player
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnPlayPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(30, 30);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(100, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "No Song";
+            // 
+            // btnPlayPause
+            // 
+            this.btnPlayPause.Location = new System.Drawing.Point(30, 80);
+            this.btnPlayPause.Name = "btnPlayPause";
+            this.btnPlayPause.Size = new System.Drawing.Size(100, 40);
+            this.btnPlayPause.TabIndex = 1;
+            this.btnPlayPause.Text = "Play/Pause";
+            this.btnPlayPause.UseVisualStyleBackColor = true;
+            this.btnPlayPause.Click += new System.EventHandler(this.btnPlayPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(150, 80);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(100, 40);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Player
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlayPause);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Player";
+            this.Text = "Player";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnPlayPause;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
---
+++ src/BitRuisseau/BitRuisseau/pages/Player.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using BitRuisseau.data;
+using NAudio.Wave;
+
+namespace BitRuisseau
+{
+    public partial class Player : Form
+    {
+        private Song currentSong;
+        private bool isPlaying = false;
+        private AudioFileReader audioFile;
+        private WaveOutEvent outputDevice;
+
+        public Player(Song song)
+        {
+            InitializeComponent();
+            currentSong = song;
+            this.Text = $"Player - {currentSong.Title}";
+            lblTitle.Text = $"{currentSong.Artist} - {currentSong.Title}";
+            PlayMusic(currentSong.Path);
+            isPlaying = true;
+            btnPlayPause.Text = "Pause";
+            
+
+        }
+
+        private void PlayMusic(string path)
+        {
+            try
+            {
+                outputDevice = new WaveOutEvent();
+                audioFile = new AudioFileReader(path);
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lecture : {ex.Message}");
+            }
+        }
+
+        private void StopMusic()
+        {
+            outputDevice.Stop();
+            isPlaying = false;
+            btnPlayPause.Text = "Play";
+        }
+
+        private void btnPlayPause_Click(object sender, EventArgs e)
+        {
+            if (isPlaying)
+            {
+                outputDevice.Pause();
+                btnPlayPause.Text = "Play";
+                isPlaying = false;
+            }
+            else
+            {
+                outputDevice.Play();
+                btnPlayPause.Text = "Pause";
+                isPlaying = true;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+             StopMusic();
+             this.Close();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+             StopMusic();
+             base.OnFormClosing(e);
+        }
+    }
+}
